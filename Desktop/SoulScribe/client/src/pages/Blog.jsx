56fd5f758d0a39c54906d3a66ce93fa991f476ed
Dhirendra <dpsingh4888@gmail.com>
2025-07-09 23:26:38 +0530

    Fix case sensitivity for Footer import

--- Desktop/SoulScribe/client/src/pages/Blog.jsx
+++ Desktop/SoulScribe/client/src/pages/Blog.jsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assets";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Moment from "moment";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import Loader from "../components/Loader";
 import { useAppContext } from "../context/AppContext";
 import toast from "react-hot-toast";
