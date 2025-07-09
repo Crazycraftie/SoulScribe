ec26ee80d6b8ba9055fe2d016c01b843fc0a39c5
Dhirendra <dpsingh4888@gmail.com>
2025-07-09 22:34:20 +0530

    Initial commit

---
+++ Desktop/SoulScribe/client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import BlogList from '../components/BlogList'
+import Newsletter from '../components/newsletter'
+import Footer from '../components/footer'
+
+
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+      <Header/>
+      <BlogList/>
+      <Newsletter/>
+      <Footer/>
+    </>
+ 
+  )
+}
+
+export default Home
